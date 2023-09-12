81860bc3328baecb10b750f5e795aa9395b61ba1
Karem1-Atef1 <karemkmbsh6@gmail.com~>
2023-09-12 05:29:20 -0400

    100

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,5 +1,9 @@
 	.file	"main.c"
 	.text
+	.section	.rodata
+.LC0:
+	.string	"Holberton School"
+	.text
 	.globl	main
 	.type	main, @function
 main:
@@ -10,6 +14,10 @@ main:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
 	movl	$0, %eax
 	popq	%rbp
 	.cfi_def_cfa 7, 8
