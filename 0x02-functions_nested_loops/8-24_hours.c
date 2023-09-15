568bbb8bd92c6f0173392ca30bcdcba4bd91aba6
Karem3tef <karemkmbsh6@gmail.com>
2023-09-14 22:55:34 -0700

    9

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,7 +9,7 @@ void jack_bauer(void)
 int mou, hou;
 for (hou = 0; hou <= 23; hou++)
 {
-for (mou = 0; mou <= 50; mou++)
+for (mou = 0; mou <= 60; mou++)
 {
 _putchar(hou / 10 + '0');
 _putchar(hou % 10 + '0');
