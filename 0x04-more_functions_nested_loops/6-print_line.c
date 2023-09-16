e5fcefd0b9db053d4ca4c35e79e92f8cbee403d3
Karem3tef <karemkmbsh6@gmail.com>
2023-09-16 04:25:07 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_line - printing lines
+ * @n: number of times
+ *
+ * Return: void
+ */
+void print_line(int n);
+{
+while (n--)
+_putchar('_');
+_putchar('\n');
+}
