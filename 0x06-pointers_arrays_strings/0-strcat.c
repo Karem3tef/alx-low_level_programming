4a74f5b27f13589410813a4e9d8b1527618a4bb9
Karem3tef <karemkmbsh6@gmail.com>
2023-09-20 07:19:53 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,11 +9,10 @@
 
 char *_strcat(char *dest, char *src)
 {
-char *str = dest;
 int i, f;
 for (i = 0; dest[i] != '\0'; i++)
-str[i] = dest[i];
+{}
 for (f = 0; src[f] != '\0'; f++, i++)
-str[i] = src[f];
-return (str);
+dest[i] = src[f];
+return (dest);
 }
