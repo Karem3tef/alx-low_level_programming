4118170dcad742f69c57f212aaa4b6cdee19ecbc
Karem3tef <karemkmbsh6@gmail.com>
2023-09-25 09:12:08 -0700

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,17 +10,18 @@
 
 void print_triangle(int size)
 {
-int i, f;
+int i, f, h;
 if (size > 0)
 {
 for (i = 1; i <= size; i++)
 {
 for (f = i ; f < size ; f++)
-_putchar(" ");
-_putchar("#");
-_putchar("\n");
+_putchar(' ');
+for (h = 1; h <= i; h++)
+_putchar('#');
+_putchar('\n');
 }
 }
 else
-_putchar("\n");
+_putchar('\n');
 }
