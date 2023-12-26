c8318b7abe9732833d2eed685cec82bd3a3f20ae
karem atef <karemkmbsh@gmail.com>
2023-12-26 06:57:52 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char i;
+	i = 'a';
+        while (i >='z')
+	{
+	putchar(i);
+	i++;
+	}
+	putchar('\n');
+	return (0);
+}
